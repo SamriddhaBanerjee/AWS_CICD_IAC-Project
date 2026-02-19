1c45ab761bad7c9e067769ca86896d803aee24d2
SamriddhaBanerjee <samriddha.ban@gmail.com>
2026-02-19 23:44:50 +0530

    Addin S3 DynamoDB and CD Ops

--- main.tf
+++ main.tf
@@ -1,18 +1,25 @@
 provider "aws" {
-  region="ap-south-1"
+  region = "ap-south-1"
 }
 resource "aws_vpc" "myCiCdAWS" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name="Git_IAC_Create"
+    Name = "Git_IAC_Create"
   }
 }
 terraform {
   backend "s3" {
-    bucket="sammy-demo-bucket-1234567"
-    key = "sam_dev/terraform.tfstate"
-    region = "ap-south-1"
+    bucket         = "sammy-demo-bucket-1234567"
+    key            = "sam_dev/terraform.tfstate"
+    region         = "ap-south-1"
     dynamodb_table = "sammy-terraform-lock-table"
-    encrypt = true
+    encrypt        = true
+  }
+  backend "s3" {
+    bucket         = "sammy-demo-bucket-1234567"
+    key            = "sam_acc/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "sammy-terraform-lock-table"
+    encrypt        = true
   }
 }
