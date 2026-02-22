e387b4e2e445572b60961cb1745731229416104a
SamriddhaBanerjee <samriddha.ban@gmail.com>
2026-02-22 11:58:30 +0530

    Dev Environment Infra and Security Groups Protocol Change

---
+++ environments/prod/main.tf
@@ -0,0 +1,43 @@
+module "vpc" {
+  source = "../../modules/vpc"
+
+  environment          = "prod"
+  vpc_cidr             = "10.1.0.0/20"
+  public_subnet_cidr_1 = "10.1.1.0/24"
+  public_subnet_cidr_2 = "10.1.2.0/24"
+  availability_zone_1  = "ap-south-1a"
+  availability_zone_2  = "ap-south-1b"
+}
+
+module "security_group" {
+  source      = "../../modules/security-groups"
+  name        = "prod-app-sg"
+  description = "production security groups"
+  vpc_id      = module.vpc.vpc_id
+  ingress_rules = [
+    {
+      description = "HTTPS"
+      from_port   = [80, 443]
+      to_port     = [80, 443]
+      protocol    = "TCP"
+    }
+  ]
+  egress_rules = [
+    {
+      description = "HTTPS"
+      from_port   = [80, 443]
+      to_port     = [80, 443]
+      protocol    = "TCP"
+    }
+  ]
+}
+
+module "ec2" {
+  source            = "../../modules/ec2"
+  name              = "prod-instance"
+  ami_id            = "ami-0bca660a856fc8c72"
+  instance_type     = "t3.micro"
+  subnet_id         = module.vpc.public_sub_1
+  security_group_id = [module.security_group.security_group_id]
+  root_volume_size  = 50
+}
